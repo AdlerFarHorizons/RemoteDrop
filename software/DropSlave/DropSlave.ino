8e1436e6420b58aefe2c1a42461e5ba0b6da1337
agilliam <ashleygilliam2011@u.northwestern.edu>
2015-01-16 11:49:34 -0600

    Added pinMode for Outputs

--- software/DropSlave/DropSlave.ino
+++ software/DropSlave/DropSlave.ino
@@ -9,6 +9,13 @@
   void setup() {
     Serial.begin(9600);
     mySerial.begin(9600);
+    pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);
+    pinMode(5, OUTPUT);
+    pinMode(6, OUTPUT);
+    pinMode(7, OUTPUT);
+    pinMode(8, OUTPUT);
   }
   
   void loop() {
